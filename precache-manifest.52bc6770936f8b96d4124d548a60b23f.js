d62274fb00734640d1f54a63d88b338f6e5faa14
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2020-11-13 08:37:48 -0800

    Add files via upload
    olx replica

---
+++ precache-manifest.52bc6770936f8b96d4124d548a60b23f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "becab5f12f642afda84471356553b1e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d50730ac2c53acc6fbc",
+    "url": "/static/css/main.e22735ce.chunk.css"
+  },
+  {
+    "revision": "939aaeb02c5572a80e05",
+    "url": "/static/js/2.64de36ec.chunk.js"
+  },
+  {
+    "revision": "a85cf428cc0fb4b6788da88d8deb99e2",
+    "url": "/static/js/2.64de36ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d50730ac2c53acc6fbc",
+    "url": "/static/js/main.5448b4ad.chunk.js"
+  },
+  {
+    "revision": "6d2ba80b347f2afa11fd",
+    "url": "/static/js/runtime-main.bf26d36c.js"
+  },
+  {
+    "revision": "9d1961ef554638c4ee1be40c00a01ac1",
+    "url": "/static/media/animal-kingdom.9d1961ef.png"
+  },
+  {
+    "revision": "7c88a25077ff87eeac772ae6c6a21d54",
+    "url": "/static/media/businessman.7c88a250.png"
+  },
+  {
+    "revision": "8a8e0f67ef7fbcec83014750f92b52ba",
+    "url": "/static/media/electronic.8a8e0f67.png"
+  },
+  {
+    "revision": "62d8b6ef8e6c796a5ddad822dff96874",
+    "url": "/static/media/facebook-brands.62d8b6ef.svg"
+  },
+  {
+    "revision": "da3a9ed4399e0dc38e09a9d494a62f28",
+    "url": "/static/media/google-brands.da3a9ed4.svg"
+  },
+  {
+    "revision": "311b7fc943f6469bbe77fe30e939cfc2",
+    "url": "/static/media/heart-regular.311b7fc9.svg"
+  },
+  {
+    "revision": "fc6dd4dd069657147508354c61c31934",
+    "url": "/static/media/olx-header.fc6dd4dd.jpg"
+  },
+  {
+    "revision": "9375a4161999e132f29e8030332fa26a",
+    "url": "/static/media/property.9375a416.png"
+  }
+]);
